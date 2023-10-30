119355316ca25eb89a0700f5011b2dd339c2bcf8
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-30 17:03:25 +0530

    Editing and Deleting Expenses

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -9,6 +9,8 @@ const ExpenseTracker = () => {
   });
   const [expensesList, setExpensesList] = useState([]);
 
+  const [editExpense, setEditExpense] = useState(null); // State to store the expense being edited
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setExpense({
@@ -18,12 +20,50 @@ const ExpenseTracker = () => {
   };
 
   const handleAddExpense = () => {
-    // Send the expense data to Firebase
+    if (editExpense) {
+      // If editExpense is not null, it means we are in edit mode
+      handleEditExpense();
+    } else {
+      // Send the expense data to Firebase
+      fetch(
+        "https://expense-trac-ad468-default-rtdb.firebaseio.com/expenses.json",
+        {
+          method: "POST",
+          body: JSON.stringify(expense),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Expense added successfully:", data);
+          setExpense({
+            amount: "",
+            description: "",
+            category: "Food",
+          });
+
+          // Update the expense list
+          setExpensesList([...expensesList, { ...expense, id: data.name }]);
+        })
+        .catch((error) => {
+          console.error("Error adding expense:", error);
+        });
+    }
+  };
+
+  const handleEditExpense = () => {
+    // Send the edited expense data to Firebase
     fetch(
-      "https://expense-trac-ad468-default-rtdb.firebaseio.com/expenses.json",
+      `https://expense-trac-ad468-default-rtdb.firebaseio.com/expenses/${editExpense.id}.json`,
       {
-        method: "POST",
-        body: JSON.stringify(expense),
+        method: "PUT",
+        body: JSON.stringify({
+          amount: expense.amount,
+          description: expense.description,
+          category: expense.category,
+        }),
         headers: {
           "Content-Type": "application/json",
         },
@@ -31,7 +71,10 @@ const ExpenseTracker = () => {
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log("Expense added successfully:", data);
+        console.log("Expense edited successfully:", data);
+
+        // Reset the edit state and clear the form
+        setEditExpense(null);
         setExpense({
           amount: "",
           description: "",
@@ -39,11 +82,44 @@ const ExpenseTracker = () => {
         });
 
         // Update the expense list
-        setExpensesList([...expensesList, { ...expense, id: data.name }]);
+        const updatedList = expensesList.map((item) =>
+          item.id === editExpense.id ? { id: item.id, ...expense } : item
+        );
+        setExpensesList(updatedList);
       })
       .catch((error) => {
-        console.error("Error adding expense:", error);
+        console.error("Error editing expense:", error);
       });
+  };
+
+  const handleDeleteExpense = (id) => {
+    // Send a DELETE request to remove the expense from Firebase
+    fetch(
+      `https://expense-trac-ad468-default-rtdb.firebaseio.com/expenses/${id}.json`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then(() => {
+        console.log("Expense successfully deleted");
+
+        // Update the expense list by removing the deleted expense
+        const updatedList = expensesList.filter((item) => item.id !== id);
+        setExpensesList(updatedList);
+      })
+      .catch((error) => {
+        console.error("Error deleting expense:", error);
+      });
+  };
+
+  const handleEditClick = (item) => {
+    // Populate the form with the data of the expense to be edited
+    setEditExpense(item);
+    setExpense({
+      amount: item.amount,
+      description: item.description,
+      category: item.category,
+    });
   };
 
   useEffect(() => {
@@ -54,7 +130,10 @@ const ExpenseTracker = () => {
       .then((response) => response.json())
       .then((data) => {
         if (data) {
-          const expenses = Object.values(data);
+          const expenses = Object.keys(data).map((key) => ({
+            id: key,
+            ...data[key],
+          }));
           setExpensesList(expenses);
         }
       })
@@ -67,7 +146,7 @@ const ExpenseTracker = () => {
     <Container>
       <Row>
         <Col md={6}>
-          <h2>Add Expenses</h2>
+          <h2>Add/Edit Expenses</h2>
           <Form>
             <Form.Group>
               <Form.Label>Amount Spent</Form.Label>
@@ -100,20 +179,45 @@ const ExpenseTracker = () => {
                 <option value="Salary">Salary</option>
               </Form.Control>
             </Form.Group>
-            <Button
-              variant="primary"
-              className="mt-2"
-              onClick={handleAddExpense}
-            >
-              Add Expense
-            </Button>
+            {editExpense ? (
+              <Button
+                variant="primary"
+                className="mt-2"
+                onClick={handleAddExpense}
+              >
+                Save
+              </Button>
+            ) : (
+              <Button
+                variant="primary"
+                className="mt-2"
+                onClick={handleAddExpense}
+              >
+                Add Expense
+              </Button>
+            )}
           </Form>
           <h2>Expenses List</h2>
           <ul>
-            {expensesList.map((item, index) => (
-              <li key={index}>
+            {expensesList.map((item) => (
+              <li key={item.id}>
                 Amount: {item.amount}, Description: {item.description},
                 Category: {item.category}
+                <Button
+                  variant="success"
+                  className="ml-2"
+                  onClick={() => handleEditClick(item)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="danger" 
+                  className="ml-2"
+                  style={{ marginLeft: '5px' }}
+                  onClick={() => handleDeleteExpense(item.id)}
+                >
+                  Delete
+                </Button>
               </li>
             ))}
           </ul>
